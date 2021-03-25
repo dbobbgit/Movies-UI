b42d14a8524913f5cfe101fa7c79fc0afbf40801
dbobbgit <63315747+dbobbgit@users.noreply.github.com>
2021-03-25 08:21:07 -0700

    renders movies to DOM, search needs work

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const API_URL =
-  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6fc70a2a598f084ddfb75487c&page=1";
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=d2093778ddc7b79d9cf3974ebfc413cd&page=1";
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
-// const SEARCH_API=
-//   "https://api.themoviedb.org/3/search/movie?api_key=
-// &query="'
+const SEARCH_API =
+  "https://api.themoviedb.org/3/search/movie?api_key=d2093778ddc7b79d9cf3974ebfc413cd&query=";
 
-const main = document.getElementById(main);
+const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
 
@@ -47,7 +46,7 @@ function showMovies(movies) {
 }
 
 function getClassByRate(vote) {
-  if (vote > +8) {
+  if (vote >= +8) {
     return "green";
   } else if (vote >= 5) {
     return "orange";
